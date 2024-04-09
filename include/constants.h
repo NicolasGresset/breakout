4527ad83a63efe881e7e7d2ad84b158b7688ad1d
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-04-09 12:54:05 +0200

    moins de magic numbers

---
+++ include/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+constexpr int WINDOW_HEIGHT = 500;
+constexpr int WINDOW_WIDTH = 800;
+
+constexpr int DOCK_HEIGHT = 50;
+constexpr int DOCK_WIDTH = 150;
+
+constexpr int PADDING = 20;
+
+#endif
