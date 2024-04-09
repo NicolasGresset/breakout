84b8abc826a55253ed050576ded630fff14ca213
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-04-09 12:35:08 +0200

    commentaire à propos du design de la classe Dock

--- include/dock.h
+++ include/dock.h
@@ -1,10 +1,22 @@
 #ifndef DOCK_H
 #define DOCK_H
 
-#include "object.h"
+#include "SDL2Window.h"
+#include "brick.h"
+#include "vector2D.h"
+#include <SDL_render.h>
 
-class Dock : public Object{
 
+/*
+A dock is semantically close to a rectangle because of its shape but also differs
+because of different methods and its ability to move
+Hence, multiple heritance shall be considered
+#todo
+*/
+class Dock : public Object {
+public:
+  Dock(SDL_Texture* texture = nullptr);
+  void draw(SDL_Renderer *renderer) const override;
 };
 
 #endif
