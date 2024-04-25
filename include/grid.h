c7e6b1a2eb454450c2b95133c5a477710069ae64
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-04-25 21:36:59 +0200

    changed to shared_ptr

--- include/grid.h
+++ include/grid.h
@@ -2,10 +2,11 @@
 #define GRID_H
 #include "gui/assets.h"
 #include "object/brick.h"
-#include "utils/constants.h"
 #include "object/rectangle_brick.h"
+#include "utils/constants.h"
 #include <SDL2/SDL_render.h>
 #include <cstddef>
+#include <memory>
 #include <vector>
 
 /*
@@ -17,13 +18,13 @@ is required
 //  on va essayer de faire une grille de rectangles pour l'instant
 class Grid {
 private:
-  std::vector<RectangleBrick> bricks_;
+  std::vector<std::shared_ptr<RectangleBrick>> bricks_;
   const int height_;
   const int width_;
   double window_height_;
   double window_width_;
 
-  Assets *assets_;
+  std::shared_ptr<Assets> assets_;
 
 public:
   void init();
@@ -32,6 +33,7 @@ private:
   RectangleBrick initializeElement(const int i, const int j);
 
 public:
+  Grid();
   /*
   // Width corresponds to the number of bricks on the width of the screen
   // Height corresponds to the number of bricks on the height of the screen
@@ -39,15 +41,16 @@ public:
   // for instance, for a 3*4 grid, call Grid(3, 4)
   // the convention is similar to matrixes
   */
-  Grid(const int height = 4, const int width = 8,
-       const double window_height = WINDOW_HEIGHT,
-       const double window_width = WINDOW_WIDTH, Assets *assets = nullptr);
+  Grid(const int height, const int width, const double window_height,
+       const double window_width, std::shared_ptr<Assets> assets);
 
-  Grid(Assets *assets);
+  Grid(std::shared_ptr<Assets> assets);
 
-  void draw(SDL_Renderer *renderer) const;
+  void draw(SDL_Renderer& renderer) const;
 
-  inline std::vector<RectangleBrick> &getBricks() { return bricks_; }
+  inline std::vector<std::shared_ptr<RectangleBrick>> &getBricks() {
+    return bricks_;
+  }
 
   inline double getWindowHeight() { return window_height_; }
   inline double getWindowWidth() { return window_width_; }
--- src/grid.cpp
+++ src/grid.cpp
@@ -1,15 +1,20 @@
 #include "grid.h"
 #include "utils/constants.h"
 #include <SDL2/SDL_render.h>
+#include <cstddef>
 #include <iostream>
 
+Grid::Grid()
+    : bricks_(), height_(), width_(), window_height_(), window_width_(),
+      assets_(){};
+
 Grid::Grid(const int height, const int width, double window_height,
-           double window_width, Assets *assets)
+           double window_width, std::shared_ptr<Assets> assets)
     : height_(height), width_(width), window_height_(window_height),
       window_width_(window_width), assets_(assets){};
 
-Grid::Grid(Assets *assets)
-    : height_(10), width_(8), window_height_(WINDOW_HEIGHT), window_width_(WINDOW_WIDTH),
+Grid::Grid(std::shared_ptr<Assets> assets)
+    : bricks_(), height_(), width_(), window_height_(), window_width_(),
       assets_(assets) {}
 
 void Grid::init() {
@@ -17,17 +22,17 @@ void Grid::init() {
   double element_width = window_width_ / width_;
   for (int i{0}; i < height_; i++) {
     for (int j{0}; j < width_; j++) {
-      bricks_.push_back(
-          RectangleBrick(Vector2D(j * element_width + element_width / 2,
-                                  i * element_height + element_height / 2),
-                         assets_->getRectangleTexture(Color::red), 1,
-                         element_width, element_height));
+      bricks_.push_back(std::make_shared<RectangleBrick>(
+          Vector2D(j * element_width + element_width / 2,
+                   i * element_height + element_height / 2),
+          assets_->getRectangleTexture(Color::red), 1, element_width,
+          element_height));
     }
   }
 }
 
-void Grid::draw(SDL_Renderer *renderer) const {
+void Grid::draw(SDL_Renderer &renderer) const {
   for (auto brick : bricks_) {
-    brick.draw(renderer);
+    brick->draw(renderer);
   }
 }
