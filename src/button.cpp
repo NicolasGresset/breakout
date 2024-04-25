d5f27ce346e97636a8d23d7cc4173dd59fff8997
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-04-25 12:39:10 +0200

    added static cast so cpp11 doesnt complain

--- src/button.cpp
+++ src/button.cpp
@@ -1,40 +1,39 @@
 #include "../include/button.h"
 #include "utils.h"
-#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_ttf.h>
 #include <cstddef>
 #include <iostream>
 
-Button::Button(Vector2D position, SDL_Color color, double width,
-               double height, std::string text)
-    : Object(position, NULL), color_(color), width_(width), height_(height), text_(text)
-{}
-;
+Button::Button(Vector2D position, SDL_Color color, double width, double height,
+               std::string text)
+    : Object(position, NULL), color_(color), width_(width), height_(height),
+      text_(text){};
 
-//Button::Button() : Object(Vector2D{0, 0}, nullptr){};
+// Button::Button() : Object(Vector2D{0, 0}, nullptr){};
 
 void Button::draw(SDL_Renderer *renderer) const {
-    Vector2D upper_left_coords = this->toUpperLeftCoords();
-    SDL_Rect destButton = {(int)upper_left_coords.x_,
-        (int)upper_left_coords.y_, (int)width_,
-        (int)height_};
-    int code;
+  Vector2D upper_left_coords = this->toUpperLeftCoords();
+  SDL_Rect destButton = {(int)upper_left_coords.x_, (int)upper_left_coords.y_,
+                         (int)width_, (int)height_};
+  int code;
 
-    TTF_Font * font = TTF_OpenFont("./font/hershey.ttf", 24);
-    checkSDLReturnCode(!font);
-    // Render text inside button
-    SDL_Surface* surface = TTF_RenderText_Solid(font, text_.c_str(), color_);
-    checkSDLReturnCode(!surface);
+  TTF_Font *font = TTF_OpenFont("./font/hershey.ttf", 24);
+  checkSDLReturnCode(!font);
+  // Render text inside button
+  SDL_Surface *surface = TTF_RenderText_Solid(font, text_.c_str(), color_);
+  checkSDLReturnCode(!surface);
 
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    checkSDLReturnCode(!texture);
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+  checkSDLReturnCode(!texture);
 
-    SDL_Rect textRect = { (int)(upper_left_coords.x_ + (width_ - surface->w) / 2),
-        (int)(upper_left_coords.y_ + (height_ - surface->h) / 2), width_, height_ };
+  SDL_Rect textRect = {(int)(upper_left_coords.x_ + (width_ - surface->w) / 2),
+                       (int)(upper_left_coords.y_ + (height_ - surface->h) / 2),
+                       static_cast<int>(width_), static_cast<int>(height_)};
 
-    TTF_CloseFont(font);
-    SDL_FreeSurface(surface);
+  TTF_CloseFont(font);
+  SDL_FreeSurface(surface);
 
-    code = SDL_RenderCopy(renderer, texture, NULL, &textRect);
-    checkSDLReturnCode(code < 0);
+  code = SDL_RenderCopy(renderer, texture, NULL, &textRect);
+  checkSDLReturnCode(code < 0);
 }
