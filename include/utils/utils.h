5606c8c5142e0b8c7b724c9e37f30532bf10e594
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-05-02 10:24:35 +0200

    improved error checking

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -5,7 +5,7 @@
 #include <iostream>
 
 /*
-Wrapper to checkSDLReturnCode to pass file and line number
+Convenient wrapper to checkSDLReturnCode to pass file and line number
 */
 #define CHECK_SDL_RETURN_CODE(code) checkSDLReturnCode(code, __FILE__, __LINE__)
 
@@ -19,7 +19,26 @@ static inline void checkSDLReturnCode(bool code, std::string file, int line) {
               << "\033[1;33m" << line << ": "
               << "\033[1;31m" << std::string{SDL_GetError()} << std::endl
               << "\033[0m";
-    exit(EXIT_FAILURE);
+    abort();
+  }
+}
+
+/*
+Convenient wrapper to assertFn to pass file and line number
+*/
+#define ASSERT_FN(code, fn) assertFn(code, fn, __FILE__, __LINE__)
+
+/**
+Allows to smoothly check for pre-conditions assertions inside of functions
+*/
+template <typename Fn>
+static inline void assertFn(bool expr, Fn fn, std::string file, int line) {
+  if (!expr) {
+    std::cerr << "\033[1;34m" << file << ":"
+              << "\033[1;33m" << line << ": "
+              << "\033[0m" << std::endl;
+    fn();
+    abort();
   }
 }
 
