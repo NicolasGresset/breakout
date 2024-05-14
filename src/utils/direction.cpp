a1d8a447c053df11b17ac22414e4b4d3239372e6
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-05-14 23:37:47 +0200

    normalized direction

--- src/utils/direction.cpp
+++ src/utils/direction.cpp
@@ -1,7 +1,13 @@
 #include "utils/direction.h"
+#include "utils/vector2D.h"
 
 Direction::Direction() {}
 
 Direction::Direction(double x, double y) : x_(x), y_(y) {}
 
 Direction::Direction(bool is_valid) : is_valid_(is_valid) {}
+
+Vector2D Direction::getVector() const {
+  double norm = sqrt(pow(x_, 2) + pow(y_, 2));
+  return Vector2D(x_ / norm, y_ / norm);
+}
