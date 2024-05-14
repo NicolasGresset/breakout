4e7662af04ee439268cae0a5ee6a676e4b52cfa6
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-05-14 21:36:14 +0200

    added dotproduct

--- include/utils/vector2D.h
+++ include/utils/vector2D.h
@@ -10,6 +10,9 @@ public:
   Vector2D(double x = 0, double y = 0);
   Vector2D(const Vector2D &vector);
 
+  inline double dotProduct(Vector2D &other) {
+    return x_ * other.x_ + y_ * other.y_;
+  }
 
   void print(void) const;
 };
