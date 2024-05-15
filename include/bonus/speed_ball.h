dc15e5aa6e6a1b366f6e54b5015fe80049c1dcff
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-05-15 05:07:41 +0200

    fixed little error

--- include/bonus/speed_ball.h
+++ include/bonus/speed_ball.h
@@ -7,11 +7,12 @@
 
 class SpeedBall : public Bonus {
   void action(Game &game) override {
+    printf("got extra speed\n");
     balls_ptr balls = game.getBall();
     for (auto ball : *balls) {
       Vector2D new_speed(ball->getSpeed());
       double norm = ball->getSpeed().getNorm();
-      double angle = tan(ball->getSpeed().y_ / ball->getSpeed().y_);
+      double angle = tan(ball->getSpeed().y_ / ball->getSpeed().x_);
       new_speed.x_ += (coefficient_ - 1) * norm * cos(angle);
       new_speed.y_ += (coefficient_ - 1) * norm * sin(angle);
       ball->setSpeed(new_speed);
